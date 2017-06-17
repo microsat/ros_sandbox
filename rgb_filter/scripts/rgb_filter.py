9f865dcf8b841117c956a298b5d3b79834c4ee2e
D. Nielsen <d.p.nielsen@gmail.com>
2017-06-16 23:24:12 -0700

    add to Cmakelist name change

--- rgb_filter/scripts/rgb_filter.py
+++ rgb_filter/scripts/rgb_filter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # filter.py
+
 import rospy
 import cv2
 import numpy as np
+
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError 
+
 from dynamic_reconfigure.server import Server
 from rgb_filter.cfg import RGBFilterConfig
 
